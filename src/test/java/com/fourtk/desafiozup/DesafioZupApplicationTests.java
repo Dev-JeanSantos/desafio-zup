218088114a92c1dc5a8895796112d5f6499e1972
Jean Santos <jeancbsan@gmail.com>
2020-12-28 16:23:31 -0300

    first commit

---
+++ src/test/java/com/fourtk/desafiozup/DesafioZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourtk.desafiozup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
